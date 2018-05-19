aa3b104c47e752c29a20f0752017b3607c8faf7e
laozhang <1638603890@qq.com>
2018-05-19 03:01:41 -0700

    interstint

--- paixu.java
+++ paixu.java
@@ -1,11 +1,17 @@
-int a [] = 1,2,3,4,5,6,7;
-for(i=o;i<a.length;i++){
-	for(j = i;j<a.length;j++){
-		if(a[i]<a[j]){
-			int temp = a[i];
-			a[i] = a[j];
-			a[j] = temp;
-}}}
-for(i = 0;i<a.length;i++){
-	System.out.print(a[i]+"  ");
+public class paixu{
+	public static void main(String[] agrs){
+		int[] arr = {12,52,63,84,75,9,24,31,85,24};
+		for(int i = 0;i <arr.length;i++){
+			for(int j = i;j< arr.length ;j++){
+				if(arr[i] < arr[j]){
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+		}
+		for(int i : arr){
+			System.out.print(i + "  ");
+		}
+	}
 }
